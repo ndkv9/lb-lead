fb256cd3c68fe6e228c0cfb0954c67f7f5bc8af9
Vu Nguuyen <vu.nguyen101@.com>
2022-01-31 14:20:08 +0200

    Modify styles

--- src/components/Lead.js
+++ src/components/Lead.js
@@ -4,34 +4,51 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#424242',
+      main: '#212121',
+      dark: '#000',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#d80711',
+      dark: '#f44336',
+      contrastText: '#fff',
+    },
+  },
+});
 
 const useStyles = makeStyles({
-  bar: {
-    backgroundColor: "#000",
-  },
   field: {
-    backgroundColor: "#FFF",
-    color: "#FFF",
-    marginLeft: "30px",
-    marginRight: "30px"
+    backgroundColor: "#fff",
+    color: "#000",
+    borderRadius: "5px",
   },
-  cancelBtn: {
-    marginLeft: "30px"
+  btn: {
+    marginLeft: "30px",
   }
 })
+
 
 
 function Lead() {
   const classes = useStyles()
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <ThemeProvider theme={theme}>
       <AppBar position="static">
-        <Toolbar className={classes.bar}>
+        <Container maxWidth="xl" color="primary">
+        <Toolbar disableGutters>
           <Avatar
             alt="Its me"
             variant="rounded"
@@ -41,37 +58,51 @@ function Lead() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{marginLeft: "20px"}}>
             Want more leads? Try our bots and forms
           </Typography>
-
           <Box
             component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
           >
-            <FormControl className={classes.field}>
-              <InputLabel htmlFor="component-outlined">Name</InputLabel>
-              <OutlinedInput
-                id="component-outlined"
-                value="name"
-                onChange={console.log}
+            <div>
+              <TextField
+                id="standard-textarea"
                 label="Name"
+                placeholder="Placeholder"
+                multiline
+                variant="filled"
+                size="small"
+                className={classes.field}
               />
-            </FormControl>
+            </div>
           </Box>
-
           <Box
             component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
           >
-            <FormControl className={classes.field}>
-              <InputLabel htmlFor="component-outlined">Email</InputLabel>
-              <OutlinedInput
-                id="component-outlined"
-                value="phone number"
-                onChange={console.log}
-                label="Name"
+            <div>
+              <TextField
+                id="standard-textarea"
+                label="Email"
+                placeholder="Placeholder"
+                multiline
+                variant="filled"
+                size="small"
+                className={classes.field}
               />
-            </FormControl>
+            </div>
           </Box>
+          <Button variant="contained" color="secondary" className={classes.btn}>Submit</Button>
         </Toolbar>
+        </Container>
       </AppBar>
-    </Box>
+    </ThemeProvider>
   );
 }
 
